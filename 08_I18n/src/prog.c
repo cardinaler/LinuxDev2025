b799a5c6989a7fb443f3aabba7953892cec15ff0
cardinaler <118291025+cardinaler@users.noreply.github.com>
2025-11-16 22:06:50 +0300

    Add files via upload

---
+++ 08_I18n/src/prog.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <locale.h>
+#include <libintl.h>
+#include <string.h>
+
+#define _(STRING) gettext(STRING)
+
+int main()
+{
+	setlocale(LC_ALL, "");
+    	bindtextdomain("prog", "../locale");
+    	textdomain("prog");
+	printf(_("Pick a number from 1 to 100.\n"));
+	int l = 0;
+	int r = 100;
+	int m;
+	char ans[64];
+	while(r - l > 1)
+	{
+		m = (l + r) / 2;
+		printf(_("Is your number is greater than %d?\n"), m);
+		printf("(YES / NO):");
+		if (!fgets(ans, sizeof(ans), stdin)) 
+		{
+			printf("\n");
+			printf(_("ERROR. Input closed\n"));
+			return 1;
+		}
+		ans[strlen(ans) - 1] = '\0';
+		if(strcmp(ans, "YES") == 0)
+		{
+			l = m;
+		}
+		else if(strcmp(ans, "NO") == 0)
+		{
+			r = m;
+		}
+		else
+		{
+			printf(_("Wrong format for answer!\n"));
+		}
+	}
+	printf(_("Your number is %d\n"), r);
+	return 0;
+}
